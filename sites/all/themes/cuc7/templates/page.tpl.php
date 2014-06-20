c15a469a7d82ad16fde27c3e147bb6b492944ba4
mhugos <margaret.hugos-gold@thestreet.com>
2014-06-20 16:57:33 -0400

    All changes to now.  Latest is for iPad fix for Main Menu

--- sites/all/themes/cuc7/templates/page.tpl.php
+++ sites/all/themes/cuc7/templates/page.tpl.php
@@ -1,33 +1,16 @@
 <div id="wrap">
     <div class="container">
 
-        <div id="fb-root"></div>
-        <div id="fb-root"></div>
-        <div id="fb-root"></div>
-
-        <?php // ---------------------------- social tags ---------------------?>
-        <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-        <script>(function(d, s, id) {
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) return;
-                js = d.createElement(s); js.id = id;
-                js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-        </script>
-        <?php // --------------------------- END  social tags ---------------------?>
-
-
+        <!------------------------------------------------------ #header-top -->
         <?php if ($page['header_top_left'] || $page['header_top_right']): ?>
-        <!-- #header-top -->
         <div id="header-top" class="sixteen columns clearfix">
             
             <?php if ($page['header_top_left'] && $page['header_top_right']) { ?>
-            <div class="one_half">
+            <div class="one_third">
             <?php print render($page['header_top_left']); ?>
             </div>
             
-            <div class="one_half last">
+            <div class="two_thirds last">
             <?php print render($page['header_top_right']); ?>
             </div>
             <?php } else { ?>
@@ -42,14 +25,14 @@
         
         <div class="clear"></div>
         
-        <!-- #header -->
+        <!------------------------------------------------------- #header -->
         <?php if ($page['header_right']) { ?>
         <div id="header" class="five columns clearfix">
 		<?php } else { ?>
         <div id="header" class="sixteen columns clearfix">   
         <?php } ?>
         <?php
-            print "<a id='header-home-link' href='" . $front_page . "'></a>";
+            print "<a id='header-home-link' href='" . $front_page . "'><img src='/sites/all/themes/cuc7/images/head-logo.jpg'></a>";
         ?>
             <div class="inner">
     
@@ -78,9 +61,9 @@
                 <?php endif; ?>
             </div>
         </div><!-- /#header -->
-        
+
+        <!----------------------------------------- #header-right -->
         <?php if ($page['header_right']) : ?>
-        <!-- #header-right -->
         <div id="header-right" class="eleven columns clearfix">
         
         	 <div class="inner">
@@ -92,58 +75,44 @@
         
         <div class="clear"></div>
         
-        <!-- #navigation -->
-        <div id="navigation" class="sixteen columns clearfix">
+        <!--------------------------------------------- #navigation -->
+        <div id="navigation" class="fourteen columns">
         
             <div class="menu-header">
             <?php if ($page['header']) : ?>
                 <?php print drupal_render($page['header']); ?>
                 <?php else : ?>
                 <?php 
-				if (module_exists('i18n_menu')) {
-				    $main_menu_tree = i18n_menu_translated_tree( variable_get( 'menu_main_links_source', '$main_menu'));
-				} else {
-				    $main_menu_tree = menu_tree( variable_get( 'menu_main_links_source', '$main_menu'));
-				} ?>
-				<div class="content">
-<!--				--><?php //print drupal_render($main_menu_tree); // this only renders child elements for current page's menu item ?>
-				<?php print drupal_render( $variables['menu_expanded']); /// This renders child elements for all menu items (per cuc7_preprocess_page())?>
-                </div>
+//				if (module_exists('i18n_menu')) {
+//				    $main_menu_tree = i18n_menu_translated_tree( variable_get( 'menu_main_links_source', '$main_menu'));
+//				} else {
+//				    $main_menu_tree = menu_tree( variable_get( 'menu_main_links_source', '$main_menu'));
+//				} ?>
+<!--				<div class="content">-->
+<!--				--><?php //print drupal_render($main_menu_tree); // MHG: this only renders child elements for current page's menu item
+//                                                    // MHG: update -- except if "show expanded" is set within the list item page in admin > menu ?>
+<!--<!--				-->--><?php ////print drupal_render( $variables['menu_expanded']); /// This renders child elements for all menu items (per cuc7_preprocess_page())
+//                                                                          //     regardless of "show as expanded" setting ?>
+        </div>
             <?php endif; ?>
             </div>
-            
         </div><!-- /#navigation -->
+        <div id="social" class="two columns clearfix">
+          <ul>
+            <li><a class="twitter" href="https://twitter.com/CUCWP" target="_blank">Twitter</a></li>
+            <li><a class="facebook" href="https://www.facebook.com/pages/Community-Unitarian-Church/117976408214797" target="_blank">Facebook</a></li>
+            <li><a class="googleplus" href="https://plus.google.com/110306245059058702672" target="_blank">Google+</a></li>
+          </ul>
+        </div>
 
-        <!-- #social-follow-tabs -->
-<!--        <div id="social-follow-tabs">-->
-<!--            <ul class="social">-->
-<!--                <li><div class="fb-like" data-href="https://www.facebook.com/pages/Community-Unitarian-Church/117976408214797" data-layout="button" data-action="like" data-show-faces="true" data-share="false"></div></li>-->
-<!--                <li><a href="https://twitter.com/CUCWP" class="twitter-follow-button" data-show-count="false" data-show-screen-name="false">Follow @CUCWP</a>-->
-<!--                    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>-->
-<!--                <li><div class="g-follow" data-href="https://plus.google.com/110306245059058702672" data-annotation="none" data-rel="publisher"></div></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-        <!-- /#social-follow-tabs -->
-        <!-- #social-tabs -->
-<!--        <div id="social-tabs">-->
-<!--            <ul class="social">-->
-<!--                <li><a href="https://twitter.com/CUCWP" class="twitter" target="_blank">Twitter</a></li>-->
-<!--                <li><a href="https://www.facebook.com/pages/Community-Unitarian-Church/117976408214797" class="facebook" target="_blank">Facebook</a></li>-->
-<!--                </li><li><a href="https://plus.google.com/110306245059058702672" class="googleplus" target="_blank">Google+</a></li>-->
-<!--            </ul>-->
-<!--        </div>-->
-        <!-- /#social-tabs -->
-
-
-
-
+        <!------------------------------------------ #sidebar-first -->
         <?php if ($page['sidebar_first']): ?>
-        <!-- #sidebar-first -->
         <div id="sidebar-first" class="five columns">
             <?php print render($page['sidebar_first']); ?>
         </div><!-- /#sidebar-first -->
         <?php endif; ?>
         
+          <!------------------------------------------ #sidebar-second -->
         <?php if ($page['sidebar_first'] && $page['sidebar_second']) { ?>
         <div id="content" class="six columns">
         <?php } elseif ($page['sidebar_first'] || $page['sidebar_second']) { ?>
@@ -152,16 +121,19 @@
         <div id="content" class="sixteen columns clearfix">    
         <?php } ?>
         
+          <!------------------------------------------ messages -->
             <?php if ($messages): ?>
                 <div id="messages">
                 <?php print $messages; ?>
                 </div><!-- /#messages -->
             <?php endif; ?>
         
+          <!------------------------------------------ #breadcrumb -->
             <?php if ($breadcrumb): ?>
-                <div id="breadcrumb"><?php print $breadcrumb; ?></div>
+                <div id="breadcrumb" class="one_half"><?php print $breadcrumb; ?></div>
             <?php endif; ?>
             
+          <!------------------------------------------ main content -->
             <div id="main">
             
                 <?php if ($page['highlighted']): ?><div id="highlighted" class="clearfix"><?php print render($page['highlighted']); ?></div><?php endif; ?>
@@ -169,9 +141,12 @@
                 <?php print render($title_prefix); ?>
                 
                 <?php if ($title && !drupal_is_front_page()): ?>
-                <h1 class="title" id="page-title">
-                  <?php print $title; ?>
+                <h1 class="title one_half" id="page-title">
+                  <?php if ($node->nid != 33): ?>  <?php // don't print title on ANNUAL GIVING page -- an image is used instead, input into body content ?>
+                    <?php print $title; ?>
+                  <?php endif; ?>
                 </h1>
+                <div class="clearfix"></div>
                 <?php endif; ?>
                 
                 <?php print render($title_suffix); ?>
@@ -253,10 +228,13 @@
                 <div class="clear"></div>
                 
                 <div id="credits">
-                    <?php print(date('Y') . ' ');?>
+                    <?php print( 'Copyright ' . date('Y') . ' ');?>
                     <?php if (!empty($site_name)):
                         print $site_name;?>
                     <?php endif;?>
+                    <?php
+                      print ('All Rights Reserved');
+                    ?>
                 </div>
         	</div>
         </div>
