d56ff645f6316f3dac6995b59313f8676e000d04
mnhugos <mhugos@verizon.net>
2014-09-27 15:19:22 -0400

    Initial

--- sites/all/modules/contrib/agenda/agenda.js
+++ sites/all/modules/contrib/agenda/agenda.js
@@ -2,27 +2,9 @@
 
   Drupal.behaviors.agenda = {
     attach: function(context, settings) {
-        $( "#hello" ).dialog({
-            autoOpen: false,
-            resizable: true,
-            modal: true,
-            overlay: {
-                backgroundColor: '#eee',
-                opacity: 0.5
-            }
-        });
-        $(".where").dialog({
-            autoOpen: false
-        })
-
-        $('#say_it').click(function () {
-            $("#hello").dialog("open");
-        });
-
-
-        $('.agenda-block .calendar_title').click(function () {
-            $(".where").dialog("open");
-        });
+      $('.agenda-block .calendar_title').click(function () {
+        $(this).next('ul').toggle('show');
+      });
     }
   };
 
