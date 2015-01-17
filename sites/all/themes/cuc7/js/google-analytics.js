902b16a46870c6b741b90c3e4cfa9baaa148c664
mnhugos <mhugos@verizon.net>
2015-01-17 14:21:00 -0500

    Add GA code

---
+++ sites/all/themes/cuc7/js/google-analytics.js
@@ -0,0 +1,17 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: mhugos
+ * Date: 1/16/15
+ * Time: 6:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-58637172-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
