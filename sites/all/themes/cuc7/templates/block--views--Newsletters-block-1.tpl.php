6bbe6969840577effb441eadeb31963848eba66d
mnhugos <mhugos@verizon.net>
2016-01-10 02:20:09 -0500

    (1) Add Audio Sermons link to Sermons block (2) Add Weekly Newsletter block to Newsletter page

---
+++ sites/all/themes/cuc7/templates/block--views--Newsletters-block-1.tpl.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * @file
+ * Customize the display of a complete webform.
+ *
+ * This file may be renamed "webform-form-[nid].tpl.php" to target a specific
+ * webform on your site. Or you can leave it "webform-form.tpl.php" to affect
+ * all webforms on your site.
+ *
+ * Available variables:
+ * - $form: The complete form array.
+ * - $nid: The node ID of the Webform.
+ *
+ * The $form array contains two main pieces:
+ * - $form['submitted']: The main content of the user-created form.
+ * - $form['details']: Internal information stored by Webform.
+ */
+?>
+<?php
+  // Print out the main part of the form.
+  // Feel free to break this up and move the pieces within the array.
+//  print 'hello there!';
+?>
+<div id="<?php print $block_html_id; ?>" class="<?php print $classes . ' seven columns'; ?>"<?php print $attributes; ?>>
+
+  <?php print render($title_prefix); ?>
+<?php if ($block->subject): ?>
+  <h2<?php print $title_attributes; ?>><?php print $block->subject ?></h2>
+<?php endif;?>
+  <?php print render($title_suffix); ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php print $content ?>
+  </div>
+</div>
